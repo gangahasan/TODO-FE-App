4bdb5746263cab6bf02e5733ea805f9488194d7d
gangahasan <rgangabhavani2@gmail.com>
2025-04-11 12:52:34 +0530

    Tried to apply responsive ness

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useSelector } from 'react-redux'
 const App = () => {
   const todos = useSelector((state)=>state.todos)
   return (
-    <div className='border-2 border-gray-400 p-2 w-150 place-self-center rounded-md flex flex-col  place-items-center'>
+    <div className='border-2  border-gray-400 p-2 w-100  md:w-150 md:p-2  sm:p-2 place-self-center rounded-md flex flex-col  place-items-center'>
       <AddTodo />
       {todos?.length >= 2 &&
         <div className='flex justify-center gap-4 w-100'>
@@ -20,5 +20,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App
